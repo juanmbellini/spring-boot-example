6c8e5320e5e95bb04c4fc9d6f1ad904bc9432cb6
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-08-14 23:59:29 -0300

    Add Spring Boot
    - Add Spring-Boot dependencies project
    - Add Spring-Boot Starter to webapi module
    - Add Spring-Boot maven plugin

---
+++ spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/config/App.java
@@ -0,0 +1,15 @@
+package com.bellotapps.examples.spring_boot_example.webapi.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bootstrap class.
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
