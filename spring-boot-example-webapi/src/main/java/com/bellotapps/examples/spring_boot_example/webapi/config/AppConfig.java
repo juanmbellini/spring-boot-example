0d0dbfca5a3efbac6e519a97568853491262666d
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-08-24 04:20:13 -0300

    Refactor Jersey configuration
    - Remove JerseyContainerBean as Spring Boot already provides one
    - Move JerseyConfig to AppConfig
    - Split Jesey needed objects creation (ObjectMapper and JacksonJaxbJsonProvider) into methods in AppConfig
    	- Tag them with Bean, in order to reuse if needed
    - Add a custom annotation (JerseyController) to be used by rest resource classes

--- spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/config/AppConfig.java
+++ spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/config/AppConfig.java
@@ -1,11 +1,21 @@
 package com.bellotapps.examples.spring_boot_example.webapi.config;
 
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.glassfish.jersey.servlet.ServletProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.ClassUtils;
+
+import javax.ws.rs.ext.Provider;
+import java.util.Arrays;
+import java.util.Collection;
 
 
 /**
@@ -18,18 +28,78 @@ import org.springframework.context.annotation.Configuration;
 })
 public class AppConfig {
 
-
     /**
-     * Jersey Servlet Container Bean configuration
+     * Creates a {@link ResourceConfig} in order to configure Jersey's behaviour.
      *
-     * @return A {@link ServletRegistrationBean} configured with Jersey's {@link ServletContainer}.
+     * @param jacksonJaxbJsonProvider The {@link Provider} used to serialize/deserialize JSON from and into POJOs.
+     * @return The {@link ResourceConfig} used to configure Jersey's behaviour.
      */
     @Bean
-    public ServletRegistrationBean jerseyServlet() {
+    @Autowired
+    public ResourceConfig jerseyConfig(JacksonJaxbJsonProvider jacksonJaxbJsonProvider) {
+        final ResourceConfig jerseyConfig = new ResourceConfig();
+        registerPackages(jerseyConfig,
+                "com.bellotapps.examples.spring_boot_example.webapi.controller",
+                "com.bellotapps.examples.spring_boot_example.webapi.exception_mappers");
+        jerseyConfig.register(jacksonJaxbJsonProvider);
 
-        final ServletRegistrationBean servletBean = new ServletRegistrationBean(new ServletContainer(), "/*");
-        servletBean.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName());
-        servletBean.setLoadOnStartup(1);
-        return servletBean;
+        return jerseyConfig;
+    }
+
+    /**
+     * Configures a {@link JacksonJaxbJsonProvider} using the given {@link ObjectMapper}
+     * and the {@link JacksonJaxbJsonProvider#DEFAULT_ANNOTATIONS}
+     * on a new {@link ObjectMapper}.
+     *
+     * @param om The {@link ObjectMapper} used to map JSON from and into POJOs.
+     * @return The configured {@link JacksonJaxbJsonProvider}.
+     */
+    @Autowired
+    @Bean
+    public JacksonJaxbJsonProvider jacksonJaxbJsonProvider(ObjectMapper om) {
+        return new JacksonJaxbJsonProvider(om, JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS);
+    }
+
+    /**
+     * Configures an {@link ObjectMapper} enabling and disabling certain
+     * {@link SerializationFeature}s and {@link DeserializationFeature}s
+     *
+     * @return The configured {@link ObjectMapper}.
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper() {
+        final ObjectMapper om = new ObjectMapper();
+        // Serialization
+        om.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+        // Deserialization
+        om.disable(DeserializationFeature.ACCEPT_FLOAT_AS_INT);
+        om.enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
+        om.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+
+        return om;
+    }
+
+
+    // ================================
+    // Helpers
+    // ================================
+
+    /**
+     * Registers the classes annotated with the {@link Provider} annotation in the given {@code packages}.
+     * This allows package scanning with Jersey (as currently not supported by library).
+     *
+     * @param packages The packages containing providers.
+     */
+    private static void registerPackages(ResourceConfig resourceConfig, String... packages) {
+        // Register packages of in app Providers
+        final ClassPathScanningCandidateComponentProvider scanner =
+                new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(Provider.class));
+
+        Arrays.stream(packages)
+                .map(scanner::findCandidateComponents).flatMap(Collection::stream)
+                .map(beanDefinition ->
+                        ClassUtils.resolveClassName(beanDefinition.getBeanClassName(), resourceConfig.getClassLoader()))
+                .forEach(resourceConfig::register);
     }
 }
