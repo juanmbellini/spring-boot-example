b6e0e0d38bd8d5a73675d157f3ea504f8412b0fc
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-08-24 03:48:15 -0300

    Add Spring Security Boot Starter
    - Add a PasswordEncoder Bean to be Autowired to the USerServiceImpl
    - Add a basic configuration, which ignores all requests

---
+++ spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.bellotapps.examples.spring_boot_example.webapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Spring Security configuration.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+}
