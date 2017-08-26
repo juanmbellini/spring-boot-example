7a2ce2ab7709a859b2b93e339900917604923048
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-08-26 04:47:10 -0300

    Fix MissingHttpStatuses enum
    - Was throwing NullPointerException when initialized

--- spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/support/constants/MissingHttpStatuses.java
+++ spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/support/constants/MissingHttpStatuses.java
@@ -15,15 +15,6 @@ public enum MissingHttpStatuses implements Response.StatusType {
     UNPROCESSABLE_ENTITY(422, "Unprocessable Entity");
 
     /**
-     * Map holding each {@link javax.ws.rs.core.Response.StatusType} declared in this enum,
-     * and in {@link javax.ws.rs.core.Response.Status} enum,
-     * together with its {@code code} as key for easy retrieval.
-     */
-    private final static Map<Integer, Response.StatusType> statuses =
-            Stream.concat(Arrays.stream(values()), Arrays.stream(Response.Status.values()))
-                    .collect(Collectors.toMap(Response.StatusType::getStatusCode, Function.identity()));
-
-    /**
      * The associated status code.
      */
     private final int code;
@@ -88,12 +79,16 @@ public enum MissingHttpStatuses implements Response.StatusType {
      * Supports {@link javax.ws.rs.core.Response.StatusType} declared in this enum,
      * and also declared in {@link javax.ws.rs.core.Response.Status} enum.
      * (using {@link javax.ws.rs.core.Response.StatusType} instead of {@link javax.ws.rs.core.Response.Status}).
-     * If no matching status in this enum, {@link javax.ws.rs.core.Response.Status#fromStatusCode(int)} will be called.
      *
      * @param statusCode The numerical status code.
      * @return The matching Status, or {@code null} is no matching Status is defined.
      */
     public static Response.StatusType fromStatusCode(final int statusCode) {
-        return statuses.get(statusCode);
+        for (Response.StatusType status : MissingHttpStatuses.values()) {
+            if (status.getStatusCode() == statusCode) {
+                return status;
+            }
+        }
+        return Response.Status.fromStatusCode(statusCode);
     }
 }
