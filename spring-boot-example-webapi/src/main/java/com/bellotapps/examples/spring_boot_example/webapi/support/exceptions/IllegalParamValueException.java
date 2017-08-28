6777382b7b1f18cc9a194e5a2edb4a8445826937
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-08-28 03:19:17 -0300

    Fix IllegalParamValueException bug
    - Could not be instantiated because of parameters issue (String, String...)

--- spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/support/exceptions/IllegalParamValueException.java
+++ spring-boot-example-webapi/src/main/java/com/bellotapps/examples/spring_boot_example/webapi/support/exceptions/IllegalParamValueException.java
@@ -1,7 +1,6 @@
 package com.bellotapps.examples.spring_boot_example.webapi.support.exceptions;
 
 import javax.ws.rs.WebApplicationException;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,35 +17,12 @@ public class IllegalParamValueException extends WebApplicationException {
     /**
      * Default constructor.
      *
-     * @param conflictingParams The param names that have been set
-     *                          with the illegal values that caused this exception to be thrown.
-     */
-    public IllegalParamValueException(String... conflictingParams) {
-        this(Arrays.asList(conflictingParams));
-    }
-
-    /**
-     * Default constructor.
-     *
      * @param conflictingParams A {@link List} containing all param names that have been set
      *                          with the illegal values that caused this exception to be thrown.
      */
     public IllegalParamValueException(List<String> conflictingParams) {
         super();
         this.conflictingParams = conflictingParams;
-    }
-
-
-    /**
-     * Constructor which can set a {@code message}.
-     *
-     * @param message           The detail message,
-     *                          which is saved for later retrieval by the {@link #getMessage()} method.
-     * @param conflictingParams The param names that have been set
-     *                          with the illegal values that caused this exception to be thrown.
-     */
-    public IllegalParamValueException(String message, String... conflictingParams) {
-        this(message, Arrays.asList(conflictingParams));
     }
 
     /**
@@ -60,18 +36,6 @@ public class IllegalParamValueException extends WebApplicationException {
     public IllegalParamValueException(String message, List<String> conflictingParams) {
         super(message);
         this.conflictingParams = conflictingParams;
-    }
-
-    /**
-     * @param message           The detail message,
-     *                          which is saved for later retrieval by the {@link #getMessage()} method.
-     * @param cause             The cause (which is saved for later retrieval by the {@link #getCause()} method).
-     *                          For more information, see {@link RuntimeException#RuntimeException(Throwable)}.
-     * @param conflictingParams The param names that have been set
-     *                          with the illegal values that caused this exception to be thrown.
-     */
-    public IllegalParamValueException(String message, Throwable cause, String... conflictingParams) {
-        this(message, cause, Arrays.asList(conflictingParams));
     }
 
     /**
